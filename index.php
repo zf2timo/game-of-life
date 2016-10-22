f1b00c49af8a205da05d3a6cbea44113fbc48cb9
Timo Tewes <timo.tewes@gmail.com>
2016-10-22 23:58:37 +0200

    Fixed some bugs

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 define('MATRIX_ROWS', 20);
-define('MATRIX_COLUMNS', 50);
+define('MATRIX_COLUMNS', 60);
 
 $matrix = [];
 for ($row = 0; $row < MATRIX_ROWS; $row++) {
@@ -24,19 +24,29 @@ $matrix[8][5] = 1;
 
 $generationCount = 10;
 for ($generation = 0; $generation < $generationCount; $generation++) {
-    $nextGenerationMatrix = clone $matrix;
+    echo str_repeat('-', 80) . PHP_EOL;
+    $nextGenerationMatrix = $matrix;
 
-    for ($row = 0; $row < MATRIX_ROWS; $row++) {
-        for ($column = 0; $column < MATRIX_COLUMNS; $column++) {
-            if ($matrix[$row][$column] === 'X') {
-                if (($matrix[$row - 1][$column - 1] + $matrix[$row - 1][$column] + $matrix[$row - 1][$column + 1] +
-                        $matrix[$row][$column - 1] + $matrix[$row][$column + 1] +
-                        $matrix[$row + 1][$column - 1] + $matrix[$row + 1][$column] + $matrix[$row + 1][$column + 1]) === 3
-                ) {
-
+    for ($row = 1; $row < MATRIX_ROWS-1; $row++) {
+        for ($column = 1; $column < MATRIX_COLUMNS-1; $column++) {
+                $livingNeighbor = $matrix[$row - 1][$column - 1] + $matrix[$row - 1][$column] + $matrix[$row - 1][$column + 1] +
+                    $matrix[$row][$column - 1] + $matrix[$row][$column + 1] +
+                    $matrix[$row + 1][$column - 1] + $matrix[$row + 1][$column] + $matrix[$row + 1][$column + 1];
+                if ($livingNeighbor === 3 || $livingNeighbor === 2) {
+                    $nextGenerationMatrix[$row][$column] = 1;
+                } elseif ($livingNeighbor === 1 || $livingNeighbor > 3) {
+                    $nextGenerationMatrix[$row][$column] = 0;
                 }
-            }
+
+
         }
+    }
+
+    for($row = 0; $row < MATRIX_ROWS; $row++) {
+        for($column = 0; $column < MATRIX_COLUMNS; $column++) {
+            echo $nextGenerationMatrix[$row][$column] === 1 ? 'X' : '.';
+        }
+        echo PHP_EOL;
     }
 
     $matrix = $nextGenerationMatrix;
